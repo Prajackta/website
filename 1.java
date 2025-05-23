d99a2c69c1a7edbb25b928c080d9aaab89c9e73a
prajacktataware <prajacktataware33@gmail.com>
2025-05-23 09:36:17 +0000

    all

--- 1.java
+++ 1.java
@@ -1,7 +1,11 @@
 Hi there:wq¡
 <<<<<<< HEAD
+<<<<<<< HEAD
 How are you?
 doing good
 =======
 hjdsjkfsdjkdsk
+>>>>>>> parent of 359f962 (new change)
+=======
+
 >>>>>>> parent of 359f962 (new change)
